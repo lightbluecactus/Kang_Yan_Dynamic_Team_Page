0a2214d2b649b3c8a166f2e9052ff7031a7f2e69
lightbluecactus <violetday77@yahoo.com>
2021-04-08 15:19:04 -0400

    finish slider banner

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 (()=>{
 
-	"use strict";
 	console.log("javascript is linked up");
+
+	const navList = document.querySelector("#navList"),
+			profileImages = document.querySelector("#profileImages");
+
+
+	function slideBanner(e) {
+		if (event.target.className.includes("navItem")) { //to avoid using forEach
+			let multiplier = event.target.dataset.offset,
+				offsetWidth = 200; 
+
+			profileImages.style.right = `${multiplier * offsetWidth}px`;
+		}
+	}
+
+	navList.addEventListener("click", slideBanner);
 
 
 })();
